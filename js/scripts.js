65c83b104f7ffe6221b98f390ce1e4d7d21b128b
emschroeder <eschroeder@engr-134-19.eastern.ewu.edu>
2014-04-30 14:39:51 -0700

    Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
 // My Scripts
 $(document).foundation();
+
+// Animate.css Stuff
+
+$('h3').click(function(){
+
+	$('h3').addClass('animated rubberBand');
+
+});
+
+// Transit.js Stuff
+
+$('img').click(function(){
+
+	// Do Stuff
+	$('p').transition({
+
+		x: 200,
+		y: 200,
+
+	});
+
+})
